1c366b5cacb61875e16f6a5a37a4a96acbbab0d5
LuciferFayte <luciferfayte@hotmail.com>
2015-11-30 22:54:55 -0800

    Revert "Fixed bounding sphere collision for MysteryBox"
    This reverts commit 1c4379c1621a0baf166282e0d3d243dfc204e7a5.

--- Source/Titanium/Titanium/Titanium/Entities/Items/MysteryBox.cs
+++ Source/Titanium/Titanium/Titanium/Entities/Items/MysteryBox.cs
@@ -44,8 +44,6 @@ namespace Titanium.Entities.Items
         SkinningData data;
         bool started = false;
 
-        private Matrix World;
-
         public MysteryBox(Vector3 position, Tile MysteryBoxTile)
         {
             // Add this to the collidables list
@@ -56,6 +54,7 @@ namespace Titanium.Entities.Items
             modelRotation = 0f;
             MBtile = MysteryBoxTile;
             BoxAmbience = BOX_DARK_AMBIENCE;
+
             setupOutcomes();
         }
 
@@ -97,12 +96,12 @@ namespace Titanium.Entities.Items
 
                 // Copy any parent transforms.
                 Matrix worldMatrix = Matrix.CreateScale(scale, scale, scale) * Matrix.CreateRotationY(modelRotation) * Matrix.CreateTranslation(ModelPos);
-             
+
                 effect.CurrentTechnique = effect.Techniques["SkinnedTech"];
 
                 // Draw the model. A model can have multiple meshes, so loop.
                 foreach (ModelMesh mesh in myModel.Meshes)
-                {                       
+                {
                     // This is where the mesh orientation is set, as well as our camera and projection.
                     foreach (ModelMeshPart part in mesh.MeshParts)
                     {
@@ -121,7 +120,6 @@ namespace Titanium.Entities.Items
                     // Draw the mesh, using the effects set above.
                     mesh.Draw();
                 }
-                
                 glow();
             }
             effect.Parameters["AmbientIntensity"].SetValue(ArenaScene.ARENA_AMBIENCE);
--- Source/Titanium/Titanium/Titanium/Utilities/PhysicsUtils.cs
+++ Source/Titanium/Titanium/Titanium/Utilities/PhysicsUtils.cs
@@ -21,38 +21,6 @@ namespace Titanium.Utilities
         /// <returns>True if the two Entities are colliding; false otherwise.</returns>
         public static bool CheckCollision(Entity a, Entity b)
         {
-            if (b.GetType() == typeof(MysteryBox))
-            {
-                return CheckCollision(a, (MysteryBox)b);
-            }
-            else
-            { 
-                if (a.myModel != null && b.myModel != null)
-                {
-                    for (int i = 0; i < a.myModel.Meshes.Count; i++)
-                    {
-                        BoundingSphere HeroSphere = a.myModel.Meshes[i].BoundingSphere;
-                        HeroSphere.Center += a.getPOSITION();
-
-                        for (int j = 0; j < b.myModel.Meshes.Count; j++)
-                        {
-                            BoundingSphere EnemySphere = b.myModel.Meshes[j].BoundingSphere;
-                            EnemySphere.Center += b.getPOSITION();
-
-                            if (HeroSphere.Intersects(EnemySphere))
-                            {
-                                //collision!
-                                return true;
-                            }
-                        }
-                    }
-                }
-                return false;
-            }
-        }
-
-        public static bool CheckCollision(Entity a, MysteryBox b)
-        {
             if (a.myModel != null && b.myModel != null)
             {
                 for (int i = 0; i < a.myModel.Meshes.Count; i++)
@@ -62,7 +30,7 @@ namespace Titanium.Utilities
 
                     for (int j = 0; j < b.myModel.Meshes.Count; j++)
                     {
-                        BoundingSphere EnemySphere = b.myModel.Meshes[j].BoundingSphere.Transform(Matrix.CreateScale(0.2f));
+                        BoundingSphere EnemySphere = b.myModel.Meshes[j].BoundingSphere;
                         EnemySphere.Center += b.getPOSITION();
 
                         if (HeroSphere.Intersects(EnemySphere))
